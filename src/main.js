a68abbdd3a2c65c714fe7a3a05375e0e2139be15
Alexandre Menezes <amenezes031@gmail.com>
2022-05-11 15:10:51 -0300

    feat: create expandable menu

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-function onScroll() {
+const SCROLL = 'scroll'
+const MENU_EXPANDED = 'menu-expanded'
+
+const onScroll = () => {
   if (scrollY > 0) {
-    navigation.classList.add('scroll')
+    navigation.classList.add(SCROLL)
   } else {
-    navigation.classList.remove('scroll')
+    navigation.classList.remove(SCROLL)
   }
+}
+
+const openMenu = () => {
+  document.body.classList.add(MENU_EXPANDED)
+}
+
+const closeMenu = () => {
+  document.body.classList.remove(MENU_EXPANDED)
 }
